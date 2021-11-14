7267072d81ad9a8ed5d40fa00237272a12c8ea37
twinkleding <32945061+TwinkleDing@users.noreply.github.com>
2021-11-14 11:28:02 +0800

    'cors跨域'

--- src/main/java/com/example/spring/WebMavConfig.java
+++ src/main/java/com/example/spring/WebMavConfig.java
@@ -18,7 +18,7 @@ public class WebMavConfig {
                         //放行哪些原始域
                         .allowedOrigins("*")
                         //是否发送Cookie信息
-                        .allowCredentials(true)
+//                        .allowCredentials(true)
                         //放行哪些原始域(请求方式)
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         //放行哪些原始域(头部信息)
